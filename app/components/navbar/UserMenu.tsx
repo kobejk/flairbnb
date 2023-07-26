f1c6ee1bd948dd881cd99d1c896dc0b9b25d76c9
kobejk <105041728+kobejk@users.noreply.github.com>
2023-07-26 16:49:36 +1000

    added placeholder image to user menu's avatar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
 
 const UserMenu = () => {
   return (
@@ -17,7 +18,9 @@ const UserMenu = () => {
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gaps-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
-          <div></div>
+          <div className="hidden md:block pl-2">
+            <Avatar />
+          </div>
         </div>
       </div>
     </div>
